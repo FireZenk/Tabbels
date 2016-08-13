8800e6095e7e66916ea264d237e22261d1da4802
firezenk <firezenk@gmail.com>
2016-08-13 21:37:26 +0200

    Apply new Action lambdas to avoid null returns

--- android/src/main/java/org/firezenk/tabbels/android/Tabbels.java
+++ android/src/main/java/org/firezenk/tabbels/android/Tabbels.java
@@ -24,38 +24,38 @@ public class Tabbels extends org.firezenk.tabbels.core.Tabbels {
         create();
         lifecycle.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
             @Override public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-                if (instance.has("onCreate"))
-                    instance.λ("onCreate", savedInstanceState);
+                if (instance.has("onCreate") && instance.isAction("onCreate"))
+                    instance.aλ("onCreate", savedInstanceState);
             }
 
             @Override public void onActivityStarted(Activity activity) {
-                if (instance.has("onStart"))
-                    instance.λ("onStart");
+                if (instance.has("onStart") && instance.isAction("onStart"))
+                    instance.aλ("onStart");
             }
 
             @Override public void onActivityResumed(Activity activity) {
-                if (instance.has("onResume"))
-                    instance.λ("onResume");
+                if (instance.has("onResume") && instance.isAction("onResume"))
+                    instance.aλ("onResume");
             }
 
             @Override public void onActivityPaused(Activity activity) {
-                if (instance.has("onPause"))
-                    instance.λ("onPause");
+                if (instance.has("onPause") && instance.isAction("onPause"))
+                    instance.aλ("onPause");
             }
 
             @Override public void onActivityStopped(Activity activity) {
-                if (instance.has("onStop"))
-                    instance.λ("onStop");
+                if (instance.has("onStop") && instance.isAction("onStop"))
+                    instance.aλ("onStop");
             }
 
             @Override public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-                if (instance.has("onSaveInstanceState"))
-                    instance.λ("onSaveInstanceState", outState);
+                if (instance.has("onSaveInstanceState") && instance.isAction("onSaveInstanceState"))
+                    instance.aλ("onSaveInstanceState", outState);
             }
 
             @Override public void onActivityDestroyed(Activity activity) {
-                if (instance.has("onDestroy"))
-                    instance.λ("onDestroy");
+                if (instance.has("onDestroy") && instance.isAction("onDestroy"))
+                    instance.aλ("onDestroy");
             }
         });
     }
